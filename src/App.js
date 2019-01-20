3447199c36ed28aaeabe8ad22149c905b4a90ae5
Kevin Lacker <lacker@gmail.com>
2019-01-19 21:52:31 -0800

    stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Hello mana simualtion world.</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/lacker/manasim"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            more information
           </a>
         </header>
       </div>

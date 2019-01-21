6af12f1ca340b014894aad51b42001e0dfe879e9
Kevin Lacker <lacker@gmail.com>
2019-01-20 21:55:25 -0800

    functional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>Hello mana simualtion world.</p>
-          <a
-            className="App-link"
-            href="https://github.com/lacker/manasim"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            more information
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default function app() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello mana simulation world.</p>
+        <a
+          className="App-link"
+          href="https://github.com/lacker/manasim"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          more information
+        </a>
+      </header>
+    </div>
+  );
 }
-
-export default App;

6bbb470af06a02f43481522ea22e49c71490b9b5
Kevin Lacker <lacker@gmail.com>
2019-02-19 09:40:38 -0800

    cleaner

--- src/App.js
+++ src/App.js
@@ -60,17 +60,7 @@ function randomPop(map) {
 export default function app() {
   return (
     <div className="App">
-      <header className="App-header">
         <p>Hello mana simulation world.</p>
-        <a
-          className="App-link"
-          href="https://github.com/lacker/manasim"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          more information
-        </a>
-      </header>
     </div>
   );
 }

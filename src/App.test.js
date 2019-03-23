697d0552b6abac536fae301f5d28c90dc2c835bb
Kevin Lacker <lacker@gmail.com>
2019-03-23 11:36:15 -0700

    t split

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
-
-it('can add', () => {
-  let left = {"foo": 1};
-  let right = {"foo": 2};
-  // TODO: fix exporty things
 });
---
+++ src/Map.test.js
@@ -0,0 +1,5 @@
+it("can add", () => {
+  let left = { foo: 1 };
+  let right = { foo: 2 };
+  // TODO: fix exporty things
+});
